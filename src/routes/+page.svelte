eb2d6aa182372bef053c1e0e3df241c337755df6
Abdullah Qadri <aq34@student.le.ac.uk>
2024-04-27 15:56:48 +0100

    Small changes. Made dark mode default

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+    import TopNavbar from "$lib/components/topNavbar.svelte";
+</script>
+
+<div class="pb-2"><TopNavbar/></div>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
