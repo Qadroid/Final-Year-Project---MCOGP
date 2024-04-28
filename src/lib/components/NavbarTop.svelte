a287a9629ab9d59235e82d229c91dc485d454eee
Abdullah Qadri <aq34@student.le.ac.uk>
2024-04-28 15:39:09 +0100

    Worked on home navbar

---
+++ src/lib/components/NavbarTop.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import * as Avatar from "$lib/components/ui/avatar/index.js";
+  import Button from "./ui/button/button.svelte";
+
+</script>
+<div class="h-14 flex justify-between bg-gray-900 text-gray-300">
+  <div id="left-menu" class="flex items-center pl-2">
+    <a href="/">
+      <img 
+        src="" 
+        alt="MCOGP Logo"
+        class="h-12 w-12 mr-4" 
+      />
+    </a>
+    <ul class="flex space-x-4">  <li>Home</li>
+      <li>Features</li>
+      <li>About</li>
+      <li>Docs</li>
+      <li>Contact</li>
+    </ul>
+  </div>
+
+  <div class="flex items-center pr-2">
+    <Button href="/auth" variant="secondary">Login</Button>
+    <Button href="/register" variant="secondary">Register</Button>
+  </div>
+  <div class="flex items-center pr-2">
+    <Avatar.Root>
+      <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+      <Avatar.Fallback>CN</Avatar.Fallback>
+    </Avatar.Root>
+    <Button href="/console" variant="secondary">Console</Button>
+  </div>
+</div>
+  
+<slot />
+  
