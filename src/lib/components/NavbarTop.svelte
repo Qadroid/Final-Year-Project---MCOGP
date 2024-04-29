71378e756ba9b6d2f1ec1c2ed995eb92d9118094
Abdullah Qadri <aq34@student.le.ac.uk>
2024-04-29 18:05:52 +0100

    More component files and working on building the console page

--- src/lib/components/NavbarTop.svelte
+++ src/lib/components/NavbarTop.svelte
@@ -12,7 +12,34 @@
 <div class="h-14 flex justify-between bg-gray-900 text-gray-300 sticky">
   <!-- Left side of Navbar -->
   <div id="left-menu" class="flex items-center pl-2">
-    <ul class="flex space-x-3">
+
+    <!-- Small devices -->
+    <div class="md:hidden flex items-center">
+      <Dialog.Root>
+        <Dialog.Trigger class="{buttonVariants({ variant: "ghost" })} h-10 r" >
+            <Anchor class="h-6 w-6" />
+          </Dialog.Trigger>
+        <Dialog.Content class="sm:max-w-screen-sm rounded-md">
+          <Dialog.Header>
+            <Dialog.Title class="text-left"></Dialog.Title>
+          </Dialog.Header>
+          <div class="flex r">
+            <ul class="text-gray-300">
+              <Button href="/" variant="ghost" class="h-10 r w-full justify-start text-xl">Home</Button>
+              <Button href="/features" variant="ghost" class="h-10 r w-full justify-start text-xl">Features</Button>
+              <Button href="/about" variant="ghost" class="h-10 r w-full justify-start text-xl">About</Button>
+              <Button href="/docs" variant="ghost" class="h-10 r w-full justify-start text-xl">Docs</Button>
+              <Button href="/contact" variant="ghost" class="h-10 r w-full justify-start text-xl">Contact</Button>
+            </ul>
+          </div>
+          <Dialog.Footer>
+          </Dialog.Footer>
+        </Dialog.Content>
+      </Dialog.Root>
+    </div>
+
+    <!-- Medium devices -->
+    <ul class="hidden md:flex space-x-3">
       <Button href="/" variant="ghost" class="h-10 r">
         <Anchor class="h-6 w-6 mr-5 ml-1 flex" />
         <h2 class="font-bold">MCOGP</h2>
@@ -43,7 +70,6 @@
           <!-- Auth/Account dialog content -->
 
           <Dialog.Footer>
-            <!-- <Button type="submit">Login</Button> -->
           </Dialog.Footer>
         </Dialog.Content>
       </Dialog.Root>
---
+++ src/routes/console/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import SidebarConsole from "$lib/components/SidebarConsole.svelte"
+
+</script>
+
+<div class="flex">
+    <!-- Sidebar -->
+    <div class=" w-72 bg-gray-950 h-full pr-12">
+        <SidebarConsole />
+    </div>
+
+    <!-- Content -->
+    <div>
+        <slot />
+    </div>
+</div>
