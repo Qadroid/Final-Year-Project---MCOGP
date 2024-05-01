2ce79bddb60c4e4b367f1d8c68e25c78f31042f7
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-01 05:39:52 +0100

    Forms may be working. Testing needed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
-<script>
+<script lang="ts">
 	import '../app.pcss';
+	import NavbarTop from '$lib/components/ui/navbar-top/+page.svelte';
 </script>
 
-<slot></slot>
+<div id="web-navbar-top" class="w-full">
+	<NavbarTop />
+</div>
+<div>
+	<slot />
+</div>
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+    import * as Tabs from "$lib/components/ui/tabs/index.js";
+    import * as Card from "$lib/components/ui/card/index.js";
+    import AuthGif from "$lib/assets/AuthGif.gif";
+
+    import LoginForm from "./login/+page.svelte";
+    import RegisterForm from "./register/+page.svelte";
+    
+    import {
+        type LoginSchema, 
+        type RegisterSchema 
+    } from "$lib/schemas/authSchema";
+    import {
+        type SuperValidated,
+        type Infer,
+    } from "sveltekit-superforms";
+	import { Button } from "@/components/ui/button";
+    
+    let loading = false;
+
+    export let data: {
+        loginForm: SuperValidated<Infer<LoginSchema>>,
+        registerForm: SuperValidated<Infer<RegisterSchema>>
+    }
+
+  </script>
+  
+  
+  <div class=" pt-40 items-center flex justify-center">
+    <div class="w-auto" />
+    <div class="rounded-xl bg-zinc-1000 flex p-6 border-solid border border-zinc-700">
+      <img src={AuthGif} class="w-[400px] h-[465px] rounded-md" alt="auth-gif"/>
+      <div class="w-full h-96 bg-gradient-to-r from-purple-500 via-cyan-500 to-red-500 background-animate"></div>
+      <div class="w-[400px] pl-3 pr-3">
+        <Tabs.Root value="login" class="w-[400px] h-[465px] pr-3">
+          <Tabs.List class="grid w-full grid-cols-2 bg-zinc-900">
+            <Tabs.Trigger value="login">Login</Tabs.Trigger>
+            <Tabs.Trigger value="register">Register</Tabs.Trigger>
+          </Tabs.List>
+          <Tabs.Content value="login">
+            <Card.Root class="h-[424px]">
+              <Card.Header>
+                <Card.Title>Login</Card.Title>
+                <Card.Description>
+                  Enter your login details below.
+                </Card.Description>
+              </Card.Header>
+              <Card.Content class="space-y-2">
+
+                <!-- Login form -->
+                <LoginForm data={data.loginForm}/>
+                
+              </Card.Content>
+              <Card.Footer>
+              </Card.Footer>
+            </Card.Root>
+          </Tabs.Content>
+          <Tabs.Content value="register">
+            <Card.Root class="h=[350px]">
+              <Card.Header>
+                <Card.Title>Register</Card.Title>
+                <Card.Description>
+                  Sign up for an account.
+                </Card.Description>
+              </Card.Header>
+              <Card.Content class="space-y-2">
+
+                <!-- Registration Form -->
+                <RegisterForm data={data.registerForm}/>
+              
+            </Card.Content>
+              <Card.Footer>
+              </Card.Footer>
+            </Card.Root>
+          </Tabs.Content>
+        </Tabs.Root>  
+      </div>
+      <div class="w-auto" />
+    </div>
+</div>
