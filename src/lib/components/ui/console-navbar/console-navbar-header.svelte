dee77e2d573bcc97aa639eac9a76100121c70df6
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-02 00:13:44 +0100

    Added Shadcn-svelte components and installed Prisma

---
+++ src/lib/components/ui/console-navbar/console-navbar-header.svelte
@@ -0,0 +1,43 @@
+<script lang=ts>
+    import * as Avatar from '$lib/components/ui/avatar/index.js';
+    import * as Select from '$lib/components/ui/select/index.js';
+	  import { Button } from '$lib/components/ui/button/index.js';
+	import { Anchor, Plus } from 'lucide-svelte';
+   
+    const projects = [
+      { value: "default", label: "default" },
+      { value: "hr", label: "hr" },
+      { value: "research", label: "research" },
+      { value: "web-app", label: "web-app" },
+      { value: "load-balancer", label: "load-balancer" }
+    ];
+</script>
+
+<div class="">
+  <!-- <p class="text-zinc-500 text-sm">Project:</p> -->
+  <Button class="w-9 h-9 p-2" variant="outline">
+    <Anchor class="" />
+  </Button>
+</div>
+<div class="">
+  <Select.Root>
+    <Select.Trigger class="w-[200px]">
+      <Select.Value placeholder="Select a project" />
+    </Select.Trigger>
+    <Select.Content>
+      <Select.Group>
+        <Select.Label class="text-zinc-400 rounded-md bg-zinc-900 text-center opacity-70">Projects</Select.Label>
+        {#each projects as project}
+          <Select.Item value={project.value} label="{project.label}"
+            >{project.label}</Select.Item
+          >
+        {/each}
+      </Select.Group>
+      <Button class="w-full mt-1" variant="outline">
+        Create new project
+        <Plus class="w-4 h-4 ml-2" />
+      </Button>
+    </Select.Content>
+    <Select.Input name="favoriteproject" />
+  </Select.Root>
+</div>
---
+++ src/lib/components/ui/console-navbar/console-navbar.svelte
@@ -0,0 +1,25 @@
+<script lang=ts>
+    import Separator from "$lib/components/ui/separator/separator.svelte";
+	import ConsoleNavbarFooter from "./console-navbar-footer.svelte";
+	import ConsoleNavbarHeader from "./console-navbar-header.svelte";
+
+</script>
+
+<div class="relative p-2 flex-col space-x-1 border-b-2 h-screen min-h-[750px] navbar-left w-72 bg-zinc-950 border-r">
+    <!-- Navbar header -->
+    <div class="h-15 flex justify-evenly  p-2 rounded-lg space-x-2 basis-full">
+        <ConsoleNavbarHeader />
+    </div>
+
+    <Separator class="w-11/12 my-1" />
+    
+    <!-- Navbar content -->
+    <div class="bg-zinc-800">
+    </div>
+
+    <!-- Navbar footer -->
+    <div class="flex flex-col navbar-left-footer absolute h-20 inset-x-0 bottom-0 space-x-3">
+        <Separator class="w-11/12 my-3"/>
+        <ConsoleNavbarFooter />
+    </div>
+</div>
