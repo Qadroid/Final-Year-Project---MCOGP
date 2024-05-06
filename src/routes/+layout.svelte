740ef184c189440df929c5e17f82b59c7fed1b1e
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-06 01:04:23 +0100

    Attempted to add auth persistence

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
 	import '../app.pcss';
 
-	import { FirebaseApp } from 'sveltefire'
+	import { FirebaseApp, userStore } from 'sveltefire'
 	import { auth, firestore } from '@/firebase';
+
+	import { onAuthStateChanged } from 'firebase/auth';
+
+	let currentUser 
+
+	onAuthStateChanged(auth, (user) => {
+	if (user) {
+		currentUser = user
+		// User is signed in
+		console.log('User is signed in');
+	} else {
+		currentUser = null
+		// User is signed out
+		console.log('User is signed out');
+	}
+	});
 
 </script>
 
