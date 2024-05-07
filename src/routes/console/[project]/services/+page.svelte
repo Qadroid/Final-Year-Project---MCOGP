43b304aabd337800b0e6b74743d699d544f581c5
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-07 11:18:39 +0100

    Added table and some logic for Services

---
+++ src/routes/console/[project]/services/+page.svelte
@@ -0,0 +1,20 @@
+<script lang=ts>
+	import { Button } from "@/components/ui/button";
+	import DataTable from "./data-table.svelte";
+	import { Plus } from "lucide-svelte";
+    export let data;
+    let { serviceList } = data
+</script>
+
+<div class="px-8 pt-3 flex justify-between items-center">
+    <h1 class="text-4xl font-bold">Services</h1>
+    <Button variant="outline" href="/console/+/services/create">
+        Create Service
+        <Plus class="h-5 w-5 ml-2" />
+    </Button>
+</div>
+
+
+<div class="container mx-auto py-10">
+    <DataTable {serviceList} />
+</div>
