565024aa41e362bd18812db73ea63753c4cc7d79
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-07 08:13:18 +0100

    Small changed in structure

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,16 +3,19 @@
 declare global {
 	namespace App {
 		// interface Error {}
+		
 		interface Locals {
 			supabase: SupabaseClient
 			safeGetSession: () => Promise<{ session: Session | null; user: User | null }>
 			session: Session | null
 			user: User | null
 		}
+		
 		interface PageData {
 			loginForm: superValidated;
 			registerForm: superValidated;
 		}
+
 		// interface LayoutData {}
 		// interface PageState {}
 		// interface Platform {}
