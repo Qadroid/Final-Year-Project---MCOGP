a4a64ff51a69d95f879e60d482929ebcfaac7b09
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-07 01:36:40 +0100

    refactored file to restrict access to /console without auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -49,12 +49,12 @@ const authGuard: Handle = async ({ event, resolve }) => {
   event.locals.session = session
   event.locals.user = user
 
-  if (!event.locals.session && event.url.pathname.startsWith('/private')) {
+  if (!event.locals.session && event.url.pathname.startsWith('/console')) {
     return redirect(303, '/auth')
   }
 
   if (event.locals.session && event.url.pathname === '/auth') {
-    return redirect(303, '/private')
+    return redirect(303, '/console')
   }
 
   return resolve(event)
