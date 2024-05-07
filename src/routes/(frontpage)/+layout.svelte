e66790779694fa33d8036cd13bc29ea3bce63808
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-07 08:18:06 +0100

    Passed through props to home navbar component

--- src/routes/(frontpage)/+layout.svelte
+++ src/routes/(frontpage)/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
     import HomeNavbar from '$lib/components/ui/home-navbar/home-navbar.svelte';
+	export let data 
+
+	let { session, supabase, user } = data
 </script>
 
 <div class="h-screen">
 	<!-- Navbar -->
 	<div id="home-navbar-top" class="w-screen h-14">
-		<HomeNavbar />
+		<HomeNavbar {session} {supabase} {user}/>
 	</div>
 	
 	<!-- Page content -->
