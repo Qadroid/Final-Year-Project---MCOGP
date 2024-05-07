7e8ad1828550be5ff9143c5d87e5c9396a87e0ae
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-07 11:18:12 +0100

    Added functions to be used in Pod creation

--- src/routes/console/[project]/pods/create/+page.server.ts
+++ src/routes/console/[project]/pods/create/+page.server.ts
@@ -37,3 +37,48 @@ export const actions: Actions = {
         })
     }
 }
+
+
+function filterNulls(obj) {
+    const result = {};
+    Object.keys(obj).forEach(key => {
+        if (obj[key] !== null && obj[key] !== undefined) {
+            if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+                const cleaned = filterNulls(obj[key]);
+                if (Object.keys(cleaned).length > 0) {
+                    result[key] = cleaned;
+                }
+            } else if (Array.isArray(obj[key])) {
+                result[key] = obj[key].map(filterNulls).filter(item => Object.keys(item).length > 0);
+            } else {
+                result[key] = obj[key];
+            }
+        }
+    });
+    return result;
+}
+
+function createPodSpec(formdata) {
+    const podSpec = {
+        metadata: {
+            name: formdata.name,
+            labels: formdata.labels
+        },
+        spec: {
+            containers: [
+                {
+                    name: formdata.name,
+                    image: formdata.image,
+                    ports: formdata.ports,
+                    env: formdata.env,
+                    volumeMounts: formdata.volumeMounts,
+                    resources: formdata.resources 
+                }
+            ],
+            volumes: formdata.volumes,
+            affinity: formdata.affinity,
+        }
+    };
+
+    return filterNulls(podSpec);
+}
