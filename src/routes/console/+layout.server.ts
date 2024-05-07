d284e0d00469c50df29363ffc953a64861907425
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-07 08:19:25 +0100

    WIP. Working on setting up auto kubernetes namspace and service account deployment

---
+++ src/routes/console/+layout.server.ts
@@ -0,0 +1,38 @@
+
+import { handleNewDefaultProject } from '@/kubernetes/kubernetes'
+import type { PageServerLoad } from './$types'
+
+export const load: PageServerLoad = async ({ depends, locals: { supabase } }) => {
+    depends('supabase:db:projects')
+    
+    const { data: projects } = await supabase
+        .from('projects')
+        .select('name', 'kubeconfig')
+
+    return {
+        projects,
+    }
+}
+
+const defaultProjectExists = async ({ depends, supabase }) => {
+    depends('supabase:db:projects')
+
+    const { data: projects } = await supabase
+        .from('projects')
+        .select('name')
+        .eq('name', 'default')
+        .single()
+
+    if (!projects) {
+        return false
+    } else {
+        return true
+    }
+}
+
+if (!defaultProjectExists) {
+    handleNewDefaultProject({ locals: { user, supabase } })
+    console.log('Default project created')
+}
+
+console.log(defaultProjectExists)
