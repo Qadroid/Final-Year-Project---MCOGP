9ca3c7db345ae8559bb7ab4441b47164536ada97
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-07 08:19:42 +0100

    Deleted unnecessary code

--- src/routes/(private)/console/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async ({ depends, locals: { supabase } }) => {
-	depends('supabase:db:projects');
-	const { data: projects } = await supabase.from('projects').select('id, name').order('name');
-	return { projects: projects || [] }
-};
