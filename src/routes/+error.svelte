5263de2cb47fe067cd42eca0a7e5716cb6c3c651
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-14 02:52:43 +0100

    Added basic error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { Construction } from "lucide-svelte";
+</script>
+
+<div class="w-full h-full items-center justify-center flex flex-col">
+    <Construction class="h-96 w-96 flex" />
+    <p class="text-2xl font-bold">ERROR: </p>
+    <p class="text-2xl">Page not found</p>
+</div>
