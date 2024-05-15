1b7a8c86a8eae3bbfd82f6c9ff6e1d82a487cc41
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-15 06:32:36 +0100

    Removed Supabase exclusive code

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,17 +1,10 @@
 // See https://kit.svelte.dev/docs/types#app
 
-import { SupabaseClient, Session, User } from "@supabase/supabase-js";
-
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		interface Locals {
-			supabase: SupabaseClient
-			safeGetSession: () => Promise<{ session: Session | null; user: User | null }>
-			session: Session | null
-			user: User | null
-		}
+		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
