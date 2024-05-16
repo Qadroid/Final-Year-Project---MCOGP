8250b4674ba6f62558ff58d0532a93e92b2a764d
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-17 00:49:31 +0100

    Added to project schema

--- src/routes/console/newProject/projectSchema.ts
+++ src/routes/console/newProject/projectSchema.ts
@@ -4,6 +4,7 @@ export const projectSchema = z.object({
     name: z.string().min(1, { message: "Project name is required" }),
     description: z.string().optional(),
     kubeConfig: z.string(),  
+    owner: z.string(),
 })
 
 export type ProjectSchema = typeof projectSchema;
