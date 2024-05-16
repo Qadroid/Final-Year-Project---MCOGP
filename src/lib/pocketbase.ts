150de6329ddbf31d821dbb945d37565f6e51a537
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-16 09:41:37 +0100

    Changed db connection to Pocketbase

---
+++ src/lib/pocketbase.ts
@@ -0,0 +1,15 @@
+import PocketBase from 'pocketbase'
+import { writable } from 'svelte/store'
+
+const pb = new PocketBase('http://127.0.0.1:8090')
+
+const currentUser = writable(pb.authStore.model)
+const authState = writable(pb.authStore.isValid)
+
+export { pb, currentUser, authState} 
+
+pb.authStore.onChange((auth) => {
+    console.log('auth changed', auth)
+    currentUser.set(pb.authStore.model)
+    authState.set(pb.authStore.isValid)
+})
