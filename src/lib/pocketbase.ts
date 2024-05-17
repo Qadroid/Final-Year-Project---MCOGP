553ea81b0f0bd8666b822ee401fd97907aa2bdc1
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-17 13:12:37 +0100

    Minor edit

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -6,9 +6,10 @@ const pb = new PocketBase('http://127.0.0.1:8090')
 const currentUser = writable(pb.authStore.model)
 const authState = writable(pb.authStore.isValid)
 
-export { pb, currentUser, authState} 
+export { pb, currentUser, authState } 
 
 pb.authStore.onChange(() => {
     currentUser.set(pb.authStore.model)
     authState.set(pb.authStore.isValid)
+
 })
