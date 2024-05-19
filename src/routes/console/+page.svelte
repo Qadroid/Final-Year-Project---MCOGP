490b1d5e6f569394b4b40754cd67f3647ddaea8d
Abdullah Qadri <aq34@student.le.ac.uk>
2024-05-19 07:16:11 +0100

    Added a little message on page for dashboard and project

--- src/routes/console/+page.svelte
+++ src/routes/console/+page.svelte
@@ -1,10 +1,6 @@
 <div class="w-full h-full items-center justify-center flex">
     <div class="text-center">
-        <p class="text-2xl font-bold">This page is currently not fully operational</p>
-        <p class="text-xl font-semibold">To see working pages, check out:</p>
-        <ul class="space-y-2 p-3 text-lg font-semibold text-cyan-200">
-            <li><a href="/console/Apps">Apps</a></li>
-            <li><a href="/console/Pods">Pods</a></li>
-        </ul>
+        <p class="text-2xl font-bold">This page is currently a placeholder and has no functionality</p>
+        <p class="text-xl font-semibold">To see working pages, check out anything in the sidebar 😁</p>
     </div>
 </div>
---
+++ src/routes/console/[project]/+page.svelte
@@ -0,0 +1,6 @@
+<div class="w-full h-full items-center justify-center flex">
+    <div class="text-center">
+        <p class="text-2xl font-bold">This page is currently a placeholder and has no functionality</p>
+        <p class="text-xl font-semibold">To see working pages, check out anything in the sidebar 😁</p>
+    </div>
+</div>
